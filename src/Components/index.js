e4633e2b48bc2a8d14a80ae360b48a43b2fec467
Pramod22nov <pramodsharma9693@gmail.com>
2024-08-12 21:37:40 +0530

    Initial commit

---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+import Todoform from "./Components/Todoform"
+import Todoitems from "./Components/Todoitems"
+
+export {Todoform,Todoitems}
---
+++ src/Index.js
@@ -0,0 +1,5 @@
+// import Todoform from "./Components/Todoform"
+// import Todoitems from "./Components/Todoitems"
+export {Todocontext , TodoProvider , useTodo} from "./Todocontext"
+
+// export {Todoform,Todoitems}
